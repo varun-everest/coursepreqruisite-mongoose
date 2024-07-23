1426c03e9bc31e0088a99f4e2f9f86bc4b0c5bf3
varun_everest <varunkumar.martha@everest.engineering>
2024-07-23 17:15:21 +0530

    Read data from file

--- Insertion/insertion.ts
+++ Insertion/insertion.ts
@@ -1,4 +1,20 @@
-import { courseModel } from "../Collections/courseprereq";
+import csvParser  from 'csv-parser';
+import fs, { read } from 'fs';
+import { courseModel } from '../Collections/courseprereq';
 
+courseModel;  //creating the course model
 
+export const readCSV = <T>(filePath: string): Promise<T[]> => {
+    return new Promise((resolve, reject) => {
+      const results: T[] = [];
+      fs.createReadStream(filePath)
+        .pipe(csvParser())
+        .on('data', (data) => results.push(data))
+        .on('end', () => resolve(results as T[]))
+        .on('error', (error: any) => reject(error));
+    });
+};
 
+export const insertData = (data: any[]) => {
+    
+}
